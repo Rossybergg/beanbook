04ecd04a4f36adb961b23ed1f388932c748b6b0c
Katie Cornish <katielcornish@gmail.com>
2019-03-07 14:12:28 +0000

    Add string array to post list activity populated with posts from user "u"

--- app/src/main/java/com/steamybeans/beanbook/PostListActivity.java
+++ app/src/main/java/com/steamybeans/beanbook/PostListActivity.java
@@ -1,29 +1,57 @@
 package com.steamybeans.beanbook;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PostListActivity extends AppCompatActivity {
+
+    TextView post;
+    DatabaseReference reff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        post = (TextView) findViewById(R.id.postContent);
+
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Users").child("u").child("posts");
+
+        reff.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                long numPostsLong = dataSnapshot.getChildrenCount();
+                int numPosts = (int) numPostsLong;
+                String[] postList = new String[numPosts];
+                for (int i = 0; i < numPosts; i++) {
+                    String childName = "post" + (i);
+                    postList[i] = dataSnapshot.child(childName).getValue().toString();
+                }
+
+                post.setText(postList[1]);
+
             }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 }
