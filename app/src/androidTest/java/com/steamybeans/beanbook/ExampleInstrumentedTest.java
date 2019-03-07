9b98d89a50c45cc22e38d4ecd7b51bad2ae0f42c
sebastianlavender <sebastianlavender@outlook.com>
2019-03-07 13:36:11 +0000

    Add feature test for SignUp

--- app/src/androidTest/java/com/steamybeans/beanbook/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/steamybeans/beanbook/ExampleInstrumentedTest.java
@@ -1,26 +1,38 @@
 package com.steamybeans.beanbook;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-/**
+/**in
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.steamybeans.beanbook", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void clickSignUpButton_toOpenSignUpPage() throws Exception {
+        onView(withId(R.id.BTNsignUp)).perform(click());
+        onView(withId(R.id.ETfullName)).perform(typeText("Joe Bloggs"));
+        onView(withId(R.id.ETemail)).perform(typeText("joe.blogs@test.com"));
+        onView(withId(R.id.ETpassword)).perform(typeText("password"));
+        onView(withId(R.id.BTNsignUp)).perform(click());
+        onView(withId(R.id.BTNlogIn)).check(matches(isDisplayed()));
     }
+
 }
