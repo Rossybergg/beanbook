28beff163c5fec9d89321eb4d390e4e455e347d0
sebastianlavender <sebastianlavender@outlook.com>
2019-03-11 15:21:47 +0000

    Correct search to search by full name

--- app/src/main/java/com/steamybeans/beanbook/AddFriendActivity.java
+++ app/src/main/java/com/steamybeans/beanbook/AddFriendActivity.java
@@ -3,7 +3,10 @@ package com.steamybeans.beanbook;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +23,7 @@ public class AddFriendActivity extends AppCompatActivity {
     private TextView TVfriends;
     private LinearLayout linearLayout;
     private DatabaseReference database;
+    private EditText ETSearchFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +37,44 @@ public class AddFriendActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        ETSearchFriend = (EditText)findViewById(R.id.ETSearchFriend);
+
+        ETSearchFriend.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                linearLayout.removeAllViews();
+                searchFriends(s.toString());
+            }
+        });
+
+
+
+    }
+
+    private void searchFriends(String search) {
         String unencodedUser = session.getUsername();
         final String user = authentication.encodeString(unencodedUser);
         System.out.println(user);
         database = FirebaseDatabase.getInstance().getReference().child("Users").child(user);
-        FirebaseDatabase.getInstance().getReference().child("Users")
+
+
+        FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("fullName").startAt(search).endAt(search + "\uf8ff")
                 .addListenerForSingleValueEvent(new ValueEventListener() {
+
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         int i = 1 ;
                         for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
 
                             TVfriends = new TextView(AddFriendActivity.this);
                             TVfriends.setText(snapshot.child("fullName").getValue() + " ");
@@ -72,7 +103,6 @@ public class AddFriendActivity extends AppCompatActivity {
                     public void onCancelled(DatabaseError databaseError) {
                     }
                 });
-
 
     }
 
