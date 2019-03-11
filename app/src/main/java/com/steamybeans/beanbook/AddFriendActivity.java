a5ec01d06ea6c3e82b692a9309027f39c3d0eb44
sebastianlavender <sebastianlavender@outlook.com>
2019-03-11 11:25:38 +0000

    Add activity add friend and nav bar link

---
+++ app/src/main/java/com/steamybeans/beanbook/AddFriendActivity.java
@@ -0,0 +1,13 @@
+package com.steamybeans.beanbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+}
