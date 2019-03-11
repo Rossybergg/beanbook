68c39f9af5de37cca7664ba7c4f9fe4ee912b42f
sebastianlavender <sebastianlavender@outlook.com>
2019-03-11 14:07:31 +0000

    Tweak Navbar

--- app/src/main/java/com/steamybeans/beanbook/Home.java
+++ app/src/main/java/com/steamybeans/beanbook/Home.java
@@ -3,16 +3,9 @@ package com.steamybeans.beanbook;
 import android.content.Intent;
 import android.graphics.Color;
 import android.icu.util.Calendar;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.solver.widgets.Snapshot;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.text.format.Time;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -21,20 +14,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Locale;
 
 public class Home extends AppCompatActivity
 
@@ -161,7 +151,7 @@ public class Home extends AppCompatActivity
 
         if (id == R.id.nav_add_friend) {
             startActivity(new Intent(Home.this, AddFriendActivity.class));
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_view_friends) {
 
         } else if (id == R.id.nav_slideshow) {
 
